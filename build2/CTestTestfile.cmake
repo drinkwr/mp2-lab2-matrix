99d3ced286d617ea8b191820ddc7474f61c669ed
drinkwr <drinkwr@yandex.ru>
2022-11-16 22:39:39 +0300

    fix

--- build2/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tas/mp2-lab2-matrix
-# Build directory: /Users/tas/mp2-lab2-matrix/build2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("include")
-subdirs("gtest")
-subdirs("test")
--- build2/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tas/mp2-lab2-matrix/test
-# Build directory: /Users/tas/mp2-lab2-matrix/build2/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_matrix "test_matrix")
-set_tests_properties(test_matrix PROPERTIES  _BACKTRACE_TRIPLES "/Users/tas/mp2-lab2-matrix/test/CMakeLists.txt;9;add_test;/Users/tas/mp2-lab2-matrix/test/CMakeLists.txt;0;")
